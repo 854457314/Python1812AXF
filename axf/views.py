ef33b68a3acff8ab123de488a46129c4683edde2
Python1812 <854457314@qq.com>
2019-03-12 19:26:18 +0800

    排序,登录，注册

--- axf/views.py
+++ axf/views.py
@@ -52,14 +52,16 @@ def market(request, childid='0', sortid='0'):
     print(index)
     # 根据index 获取 对应的 分类ID
     categoryid = foodtypes[index].typeid
-    # 根据 分类ID 获取对应分类信息
-    # goods_list = Goods.objects.filter(categoryid=categoryid)
+    print(categoryid)
 
     # 子类
     if childid == '0':
         goods_list = Goods.objects.filter(categoryid=categoryid)
     else:
         goods_list = Goods.objects.filter(categoryid=categoryid).filter(childcid=childid)
+
+    print(goods_list)
+    print(type(goods_list))
 
     # 排序
     # 0默认综合排序   1销量排序     2价格最低   3价格最高
@@ -72,6 +74,7 @@ def market(request, childid='0', sortid='0'):
 
     # 获取子类信息
     childtypenames = foodtypes[index].childtypenames
+    print(childtypenames)
     # 存储子类信息 列表
     childtype_list = []
     # 将对应的子类拆分出来
@@ -80,17 +83,18 @@ def market(request, childid='0', sortid='0'):
         item_arr = item.split(':')
         temp_dir = {
             'name': item_arr[0],
-            'id': item_arr[1]
+            'id': item_arr[1],
         }
 
         childtype_list.append(temp_dir)
-
+    print(childtype_list)
+    print(type(childtype_list))
 
     response_dir = {
         'foodtypes': foodtypes,
         'goods_list': goods_list,
         'childtype_list': childtype_list,
-        'childid': childid
+        'childid': childid,
     }
 
 
@@ -106,6 +110,7 @@ def mine(request):
     token = request.session.get('token')
     userid = cache.get(token)
     user = None
+
     if userid:
         user = User.objects.get(pk=userid)
 
@@ -152,7 +157,27 @@ def register(request):
 
 
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'GET':
+        return render(request,'login.html')
+    elif request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        users = User.objects.filter(email=email)
+        if  users.exists():
+            user = users.first()
+            if user.password == generate_password(password):
+            # 状态保持
+                token = generate_token()
+                cache.set(token,user.id,60*60*24)
+            # 传递给客户端
+                request.session['token'] = token
+
+                return  redirect('axf:mine')
+            else:
+                return render(request, 'login.html', context={'pass_err': '对不起，您输入的密码错误'})
+        else:   #密码错误
+                return render(request,'login.html',context={'pass_err':'对不起,您输入的用户不存在'})
 
 
 def logout(request):
